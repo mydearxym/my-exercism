d39bd8b53ea10b86c82417f2c5a0815d38b4b097
mydearxym <mydearxym@qq.com>
2017-12-01 17:50:38 +0800

    test curry in elixir

---
+++ elixir/protein-translation/protein_translation.exs
@@ -0,0 +1,120 @@
+defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: { atom,  list(String.t()) }
+  def of_rna(rna) do
+    rna
+    |> String.to_charlist
+    |> Enum.chunk_every(3)
+    |> Enum.map(& get_codon(&1))
+    |> Enum.take_while(R.not_equal("STOP"))
+    |> datapack
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: { atom, String.t() }
+  def of_codon(codon) do
+    # case Map.fetch(@proteins, codon) do
+    # codon
+    # |> R.get_codon(@proteins)
+    # |> IO.inspect(label: "nihao")
+
+    case get_codon(codon) do
+      nil -> {:error, "invalid codon"}
+      protein -> {:ok, protein}
+    end
+      # sentence when byte_size(sentence) > 3 -> { :ok, ~w(Methionine Phenylalanine Tryptophan) }
+  end
+
+  defp datapack(value) do
+    cond do
+      is_binary(value)  -> Tuple.append({:ok}, value)
+      any_nil?(value)   -> {:error, "invalid RNA"}
+      true              -> Tuple.append({:ok}, value)
+    end
+  end
+
+  defp get_codon(codon) do
+    @proteins
+    |> Map.get(to_string(codon))
+  end
+
+  defp any_nil?(val) do
+    val
+    |> Enum.any?(R.is_nil)
+  end
+end
+
+
+# http://blog.patrikstorm.com/function-currying-in-elixir
+defmodule R do
+  def not_equal(target) do
+    R.curry(
+      fn source ->
+        target != source
+      end
+    )
+  end
+
+  def is_nil() do
+    R.curry(
+      fn val ->
+        is_nil(val)
+      end
+    )
+  end
+
+  def curry(fun) when is_function(fun) do
+    {_, arity} = :erlang.fun_info(fun, :arity)
+    curry(fun, arity, [])
+  end
+
+  def curry(fun, 0, arguments) when is_function(fun) do
+    apply(fun, Enum.reverse arguments)
+  end
+
+  def curry(fun, arity, arguments) when is_function(fun) do
+    fn arg -> curry(fun, arity - 1, [arg | arguments]) end
+  end
+end
